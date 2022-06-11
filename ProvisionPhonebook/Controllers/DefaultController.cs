f897e132bcfba40863d8f3462d8369b4f327dd5b
Tolga Kaan Eroglu <tolgakaaneroglu@gmail.com>
2022-06-11 18:35:57 +0300

    Add project files.

---
+++ ProvisionPhonebook/Controllers/DefaultController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProvisionPhonebook.Models.EntityFramework;
+
+namespace ProvisionPhonebook.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbProvisionEntities db = new DbProvisionEntities();
+        public ActionResult Index()
+        {
+            var kisi = db.TblKisi.ToList();
+            return View(kisi);
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+
+            return View();
+        }   
+        [HttpPost]
+        public ActionResult Add(TblKisi a)
+        {
+            db.TblKisi.Add(a);
+            db.SaveChanges();
+            return View();
+        } 
+
+
+        public ActionResult Delete(int id)
+        {
+            var kisi = db.TblKisi.Find(id);
+            db.TblKisi.Remove(kisi);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        } 
+        public ActionResult Bring(int id)
+        {
+            var kisi = db.TblKisi.Find(id);
+            return View("Bring", kisi);
+        } 
+        public ActionResult Update(TblKisi a)
+        {
+            var kisi = db.TblKisi.Find(a.KisiID);
+            kisi.KisiAd = a.KisiAd;
+            kisi.KisiSoyad = a.KisiSoyad;
+            kisi.KisiTel = a.KisiTel;
+            kisi.KisiTelTip = a.KisiTelTip;
+            db.SaveChanges();
+            return RedirectToAction("Index", "Default");
+        }
+
+    }
+}
